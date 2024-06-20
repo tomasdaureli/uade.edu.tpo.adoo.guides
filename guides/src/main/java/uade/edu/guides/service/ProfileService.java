b7bf2139b3e59ecf634abaa23e85d95958488088
Repee2 <francorep2@gmail.com>
2024-06-20 13:26:57 -0300

    Interfaz Profile / Profile Controller

--- guides/src/main/java/uade/edu/guides/service/ProfileService.java
+++ guides/src/main/java/uade/edu/guides/service/ProfileService.java
@@ -1,9 +1,15 @@
 package uade.edu.guides.service;
 
 import java.util.List;
+import java.util.Date;
 
 import uade.edu.guides.domain.*;
+import uade.edu.guides.entity.Guide;
+import uade.edu.guides.entity.Review;
+import uade.edu.guides.entity.Trip;
+import uade.edu.guides.entity.Trophy;
 import uade.edu.guides.service.auth.IEstrategiaAutenticacion;
+
 
 public interface ProfileService {
 
@@ -11,7 +17,23 @@ public interface ProfileService {
 
     ProfileResponseDTO createUser(CreateProfileDTO dto);
 
+    ProfileResponseDTO getProfileByDNI(String DNI);
+
     ProfileResponseDTO updateProfile(UpdateProfileDTO dto);
+
+    boolean verifyCredential(String credentialId);
+
+    void addReview(Guide guide,Review review);
+
+    void addTrophy(Guide guide, Trophy trophy);
+
+    List<Trophy> getAllTrophies(Guide guide);
+
+    boolean checkAvailability(Guide guide, Date startDate, Date endDate);
+
+    double calculateScore(List<Review> reviews);
+
+    List<TripDTO> addTripToHistory(TripDTO trip);
 
     void autenticarUsuario(AuthenticateUserDTO dto);
 

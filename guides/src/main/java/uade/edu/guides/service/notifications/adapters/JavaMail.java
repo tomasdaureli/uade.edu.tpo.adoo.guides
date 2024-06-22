1a1557c2ceb6108f4da16a7cf211331ef35f518f
Repee2 <francorep2@gmail.com>
2024-06-22 20:58:57 -0300

    Notificacion-JavaMail

--- guides/src/main/java/uade/edu/guides/service/notifications/adapters/JavaMail.java
+++ guides/src/main/java/uade/edu/guides/service/notifications/adapters/JavaMail.java
@@ -2,16 +2,17 @@ package uade.edu.guides.service.notifications.adapters;
 
 import org.springframework.stereotype.Component;
 
+import lombok.extern.slf4j.Slf4j;
 import uade.edu.guides.domain.NotificacionDTO;
 import uade.edu.guides.service.notifications.IAdapterMail;
 
 @Component
+@Slf4j
 public class JavaMail implements IAdapterMail {
 
     @Override
     public void enviarNotificacion(NotificacionDTO notificacion) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'enviarNotificacion'");
+        log.info("Se envio una notificacion a " + notificacion.getReceptor().getName() + " " + notificacion.getReceptor().getLastName() + "\n Asunto: " + notificacion.getDescripcion() + "\nNotificacion enviada via JavaMail");
     }
 
 }

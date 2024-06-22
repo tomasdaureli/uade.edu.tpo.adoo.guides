2cff6b9bf0b76c5c2d7226e244cc7704027d753c
Repee2 <francorep2@gmail.com>
2024-06-22 20:59:15 -0300

    Obersver-method

--- guides/src/main/java/uade/edu/guides/service/observ/IObserver.java
+++ guides/src/main/java/uade/edu/guides/service/observ/IObserver.java
@@ -1,10 +1,11 @@
 package uade.edu.guides.service.observ;
 
+import uade.edu.guides.domain.TrophyDTO;
 import uade.edu.guides.entity.Guide;
 import uade.edu.guides.entity.Trophy;
 
 public interface IObserver {
 
-    void addTrophy(Guide guideDto, Trophy trophyDto);
+    void addTrophyGuide(Long guideId, TrophyDTO trophyDto);
 
 }

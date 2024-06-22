182b460ed4c6bb2b2ab27c15eabcbb7d90b84cc9
Repee2 <francorep2@gmail.com>
2024-06-22 20:57:35 -0300

    AdapterIA-method-logic

--- guides/src/main/java/uade/edu/guides/service/credent/VerificadorIA.java
+++ guides/src/main/java/uade/edu/guides/service/credent/VerificadorIA.java
@@ -1,14 +1,30 @@
 package uade.edu.guides.service.credent;
 
+import java.util.Random;
+
 import org.springframework.stereotype.Component;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Component
 public class VerificadorIA implements IAdapterIA {
 
     @Override
-    public boolean verifyCredential(String credentialId) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'autenticarUsuario'");
+    public Boolean verifyCredential(String credentialId) {
+        Random random = new Random();
+        int randomNumber = random.nextInt(2);
+        switch (randomNumber) {
+            case 0:
+                log.info("Credencial válida: {}", credentialId);
+                return true;
+            case 1:
+                log.info("Credencial inválida: {}", credentialId);
+                return false;
+            default:
+                throw new IllegalStateException("Número aleatorio fuera de rango");
+        }
     }
+
 
 }

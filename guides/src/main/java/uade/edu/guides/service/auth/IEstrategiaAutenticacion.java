e3aac1b972c42dac9567efe7e535173260df84c1
Tomas D'Aureli <72536433+tomasdaureli@users.noreply.github.com>
2024-06-23 14:27:05 -0300

    clear code (#13)

--- guides/src/main/java/uade/edu/guides/service/auth/IEstrategiaAutenticacion.java
+++ guides/src/main/java/uade/edu/guides/service/auth/IEstrategiaAutenticacion.java
@@ -1,6 +1,5 @@
 package uade.edu.guides.service.auth;
 
-import uade.edu.guides.domain.AuthenticateUserDTO;
 import uade.edu.guides.domain.ProfileResponseDTO;
 
 public interface IEstrategiaAutenticacion {

e3be3c1128d763a637389da4b8393614a1f0f6c0
Tomas D'Aureli <72536433+tomasdaureli@users.noreply.github.com>
2024-06-25 10:23:06 -0300

    Fixes (#17)
    * cambios auth
    
    * varios cambios
    
    * varios cambios
    
    * add property to dto
    
    * add property

--- guides/src/main/java/uade/edu/guides/domain/BookDTO.java
+++ guides/src/main/java/uade/edu/guides/domain/BookDTO.java
@@ -1,5 +1,7 @@
 package uade.edu.guides.domain;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,6 +11,7 @@ import lombok.Setter;
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class BookDTO {
 
     private Long id;
